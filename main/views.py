4b5ef16d883dc5153ec96332c2c409eef5bc8e84
jeremy Vangansberg <jeremy.vangansberg@gmail.com>
2023-02-27 12:18:55 +0100

    adding bootstrap

--- main/views.py
+++ main/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home_page(request):
+    context = {
+        "test" : "Ceci est un test",
+        "fake_dict": {"key1": "valeur de la clé 1"}
+        }
+    return render(request, 'main/home_page.html', context=context)
+
+
+def about(request):
+    return render(request, 'main/about.html')
